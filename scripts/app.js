e246a18e81eb3b396e6d4bae6eea5c2133bd2594
simbrar1 <sim.brar1@hotmail.com>
2019-06-04 15:37:14 +0100

    styling

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,6 @@ let direction = null
 // use math.floor for player to start on the 12th sq. Or just put 0 for them to start at 0
 let playerShouldMove = false
 let playerScore = 0
-// const snake = [1,2]
 let playerScoreBoard = null
 let sound = null
 let foodEaten = 0
@@ -23,23 +22,27 @@ function movePlayer() {
     console.log(foodEaten)
     newFood()
   }
+  //snake getting bigger
   squares[playerIndex].classList.add('player')
   for (let i = 0; i < snake.length; i++) {
     squares[snake[i]].classList.add('player')
   }
 }
 
+//new food generating at random
 function newFood() {
   const imageUrl = changePic()
   const newSquare = squares[Math.floor(Math.random() * width * width)]
   newSquare.classList.add('food', imageUrl)
 }
 
+//func to change the food photos
 function changePic() {
   const foodPics = ['food-one', 'food-two', 'food-three', 'food-four', 'food-five', 'food-six', 'food-seven']
   return foodPics[Math.floor(Math.random() * foodPics.length)]
 }
 
+//score function
 function score() {
   console.log('score')
   playerScore++
@@ -47,6 +50,7 @@ function score() {
   // result.innerHTML
 }
 
+//food gets eaten noises
 function newSound() {
   const sound = ['assets/Whoah-NiceOne-Studio.wav', 'assets/TastesLikeChicken.wav', 'assets/PiquantWithAPleasantCrunch.wav', 'assets/SlimyYetSatisfyingSimba.wav', 'assets/SlimyYetSatisfyingPumbaa.wav', 'assets/Eeew-Gross-Studio.wav']
   return sound[Math.floor(Math.random() * sound.length)]
@@ -100,6 +104,7 @@ function handleDirection() {
   if (playerShouldMove) movePlayer()
 }
 
+//how fast the snake is moving
 setInterval(handleDirection, 300)
 
 function init() {
@@ -119,9 +124,6 @@ function init() {
   }
   squares[playerIndex].classList.add('player')
 
-  // squares.forEach((square, index) => {
-  //   if (snake.includes(index)) square.classList.add('player')
-  // })
   newFood()
   window.addEventListener('keydown', handleKeyDown)
 }
